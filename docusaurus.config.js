9cac6447a735341b3549ca53ebbe285a9e2a5c44
ImNotAOwl <siegel.matthieu@ymail.com>
2023-10-26 21:07:08 +0200

    to deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,6 @@ const config = {
   tagline: "Notes As Code is cool !",
   favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
   url: "https://notes-as-code.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
